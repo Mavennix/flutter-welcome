acbabc4bd4885d5242b5f02706d305f9a6d31627
Joseph Ajayi <jajayi@gidimobile.com>
2019-09-26 12:48:53 +0100

    initial commit

---
+++ lib/items.dart
@@ -0,0 +1,32 @@
+List items = [
+  {
+    "header": "Learn",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/2.png"
+  },
+  {
+    "header": "Build",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/3.png"
+  },
+  {
+    "header": "Launch",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/1.png"
+  },
+  {
+    "header": "Invest",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/5.png"
+  },
+  {
+    "header": "Travel",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/4.png"
+  }
+];
